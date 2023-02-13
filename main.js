b8b3c3a61de009cdf52a4b6fc02fb9b38b371806
david abraham <davidabrahamtellezbautista@hotmail.com>
2023-02-13 16:46:37 -0600

    se implementa la misma funcionalidad En python y node

--- main.js
+++ main.js
@@ -1,57 +1,30 @@
-const readline = require('readline');
-const axios = require('axios');
-
+const { Configuration, OpenAIApi } = require("openai");
 const config = require('./config.json');
-const API_KEY = config.openai.api_key;
+const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-const BASE_URL = 'https://api.openai.com/v1/engines/';
-const ENGINE = 'curie'; // Puedes cambiar "davinci" por "curie", "babbage", "ada", o "text-davinci-002"
-
-const prompt = async (message) => {
-  return new Promise(resolve => {
-    rl.question(message, (answer) => {
-      resolve(answer);
-    });
+async function obtenerCompletado() {
+  const configuration = new Configuration({
+    apiKey: config.openai.api_key,
   });
-};
+  const openai = new OpenAIApi(configuration);
 
-const getResponse = async (prompt, API_KEY) => {
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${API_KEY}`
-  };
-  const data = {
-    'prompt': prompt,
-    'temperature': 0.3,
-    'max_tokens': 20000,
-    'top_p': 1,
-    'frequency_penalty': 0,
-    'presence_penalty': 0
-  };
-  try {
-    const response = await axios.post(`${BASE_URL}${ENGINE}/completions`, data, { headers });
-    return response.data.choices[0].text.trim();
-  } catch (error) {
-    console.error(error);
-    return 'Error: could not get response from API';
-  }
-};
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-const main = async () => {
   while (true) {
-    try {
-      const question = await prompt('Enter a question: ');
-      const response = await getResponse(question, API_KEY);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-};
+    const prompt = await new Promise(resolve => {
+      rl.question('Ingresa tu prompt: ', resolve);
+    });
 
-main();
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+    });
+
+    console.log(completion.data.choices[0].text);
+  }
+}
+
+obtenerCompletado();
